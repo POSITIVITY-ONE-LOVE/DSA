c2274cbc40e753892759981be866a26a05a5e4be
CYBER OTHY <othienodavid45@gmail.com>
2025-10-25 11:28:42 +0300

    Add files via upload

---
+++ addding_items_in_lists.py
@@ -0,0 +1,24 @@
+# here basically we r going to see the different ways of adding items in lists.
+
+# 1. append() one item
+# 2. insert() one item with its index.
+# 3. extend() multiple items
+
+
+# 1. append() 
+list1 = [1,2,3,4,5]
+list1.append(6)
+print(list1)
+
+# 2. insert()
+list2 = [1,2,3,4,5]
+list2.insert(0,0)
+print(list2)
+
+# 3. extend()
+list3 = [1,2,3,4,5]
+list3.extend([6,7,8])
+print(list3)
+
+
+
